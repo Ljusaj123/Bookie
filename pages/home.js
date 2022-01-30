43ce9412416d21583d92a54f16f4740707f4d878
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-01-30 21:59:46 +0100

    file u kojem se poziva footer

---
+++ pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from '../modules/footer';
+
+const Home = () => {
+    return (
+        <div>
+            <Footer />
+
+        </div>
+    )
+}
+
+export default Home;
