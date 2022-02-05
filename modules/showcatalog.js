8fdc9260042a2cdf34c1670c0ea9117e60325998
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-02-05 23:45:05 +0100

    html struktura za showcatalog

---
+++ modules/showcatalog.js
@@ -0,0 +1,14 @@
+const ShowCatalog = () => {
+    return (
+        <section className="newsletter">
+            <div className="newsletter-container">
+                <h1>See more of interesting books</h1>
+                <button>
+                    Show catalog
+                </button>
+            </div>
+        </section>
+    );
+}
+
+export default ShowCatalog;
