9e3c17e57e7fb122d196c19bc6feb013461ad5be
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-02-06 00:09:53 +0100

    uključena komponenta newsletter

--- pages/home.js
+++ pages/home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Footer from '../modules/footer';
 import ShowCatalog from '../modules/showcatalog';
+import Newsletter from '../modules/newsletter';
 
 const Home = () => {
     return (
         <div>
             <ShowCatalog />
+            <Newsletter />
             <Footer />
 
         </div>
