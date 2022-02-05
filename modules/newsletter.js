df764c1e095d107951e93ec7961baf79f1fcfba3
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-02-06 00:09:28 +0100

    komponenta za newsletter

---
+++ modules/newsletter.js
@@ -0,0 +1,18 @@
+const Newsletter = () => {
+    return (
+        <section className="newsletter">
+            <div className="newsletter-container">
+                <h1>Sign up to our newsletter</h1>
+                <div className="sign-in">
+                    <input type="email" />
+                    <button>
+                        Sign Up
+                    </button>
+                </div>
+            </div>
+
+        </section>
+    )
+}
+
+export default Newsletter;
