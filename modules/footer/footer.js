191a67f2cf85ca62af1ca685728a6eecbd0dd656
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-02-06 20:17:35 +0100

    js datoteke postavljene u foldere, u footer dodana ikona srca

---
+++ modules/footer/footer.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { FaFacebook, FaWhatsapp, FaInstagram, FaLinkedinIn, FaHeart } from 'react-icons/fa';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-container">
+                <div className="bookie-footer">
+                    <h1>Bookie</h1>
+                    <h2>Powered by Vercel</h2>
+                </div>
+                <div className="contact-footer">
+                    <h1>Contact Us</h1>
+                    <form className="contact-us-form">
+                        <input type="text" placeholder="your name..." />
+                        <input type="email" placeholder="youremail@mail.com" />
+                        <textarea placeholder="your message here...">
+                        </textarea>
+                        <button className="send-btn">
+                            Send
+                        </button>
+                    </form>
+
+                </div>
+                <div className="made-by-footer">
+                    <h1>Made with <span><FaHeart /></span> by Rosmarina</h1>
+                    <div className="social-icons-footer">
+                        <a href=""><FaFacebook /></a>
+                        <a href=""><FaInstagram /></a>
+                        <a href=""><FaWhatsapp /></a>
+                        <a href=""><FaLinkedinIn /></a>
+                    </div>
+                </div>
+
+
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ modules/showcatalog/showcatalog.js
@@ -0,0 +1,14 @@
+const ShowCatalog = () => {
+    return (
+        <section className="show-catalog">
+            <div className="show-catalog-container">
+                <h1>See more of interesting books</h1>
+                <button>
+                    Show catalog
+                </button>
+            </div>
+        </section>
+    );
+}
+
+export default ShowCatalog;
