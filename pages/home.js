b922fc133409b89ff4ca9c17b94d49ba39008294
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-02-10 14:39:26 +0100

    uklonjen duplikat

--- pages/home.js
+++ pages/home.js
@@ -1,5 +1,4 @@
 import Header from "../modules/header/Header";
-import Footer from "../modules/footer/Footer";
 import Booklist from "../components/Booklist";
 import Footer from "../modules/footer/Footer";
 
