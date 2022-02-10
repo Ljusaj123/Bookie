9a71b8626a37b6af232aa7a50ac036070a89b3cc
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-02-10 14:53:37 +0100

    show catalog prebacen u folder, dodan novi put kod home dokumenta

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,7 @@
 import Header from "../modules/header/Header";
 import Booklist from "../components/Booklist";
 import Footer from "../modules/footer/Footer";
+import ShowCatalog from "../modules/show-catalog/showcatalog";
 
 
 const Home = () => {
